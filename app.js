c1e1664936c18c7ea82dcfd3bead9004cd872fe6
zwoolfolk <woolfolz@oregonstate.edu>
2017-07-13 17:23:04 -0400

    updated reqs
    added promises, cheerio

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var request = require('request');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cheerio = require('cheerio');
+var fs = require('fs');
 var API = require('qpx-express');
 var apiKey = 'AIzaSyDYJze67GhAHYU-f373MnZ0N1sCaDaNQHE';
 var qpx = new API(apiKey);
-
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
@@ -18,7 +19,7 @@ app.use(express.static('public'));
 
 var body = {
 	"request": {
-		"passengers": { "adultCount": 1 },
+		"passengers": { "adultCount": 2 },
 		"solutions": 1,
 		"slice": [{
 				"origin":"IND",
@@ -38,19 +39,53 @@ var body = {
 
 app.get('/', function(req, res, next){
 	var context = {};
-	qpx.getInfo(body, function(error, data) {
-		var priceStr = data.trips.tripOption[0].saleTotal;
-		priceStr = priceStr.slice(3, priceStr.length);
-		priceStr = Number(priceStr);
-		//add here, eg
-		priceStr = priceStr.toFixed(2);
-		context.flightPrice = priceStr;
-		console.log(data.trips.tripOption[0].saleTotal);
+
+	//get flight price via QPX API
+	var p1 = function(){ 
+		return new Promise(function(resolve, reject){
+			qpx.getInfo(body, function(error, data) {
+				if(error){
+					reject(error);
+				} else {
+					var priceStr = data.trips.tripOption[0].saleTotal;
+					priceStr = priceStr.slice(3, priceStr.length);
+					priceStr = Number(priceStr);
+					//add here, eg
+					priceStr = priceStr.toFixed(2);
+					context.flightPrice = priceStr;
+					//console.log(data.trips.tripOption[0].saleTotal);
+					resolve(priceStr);
+				}
+			});
+		});
+	};
+
+	//get hotel price via scraping w/ cheerio
+	var url = 'https://www.expedia.com/Hotel-Search-Data?responsive=true&destination=Sint+Maarten+(Dutch),+Sint+Maarten&startDate=02/12/2018&endDate=02/16/2018&adults=2&regionId=602298&sort=price&timezoneOffset=-14400000&siteid=1&langid=1033&hsrIdentifier=HSR&?1499891962553';
+	var p2 = function(){
+		return new Promise(function(resolve, reject){
+			request(url, function(err, resp, bod){
+				if(err){
+					reject(err);
+				} else {
+					var $ = cheerio.load(bod);
+					//var price = $('.price-breakdown-tooltip.actualPrice.price').text();
+					var price = $('.guestRatingText.secondary').text();
+					//var priceText = price.text();
+					context.stuff = bod;
+					console.log(price);
+					resolve(price);
+				}
+			});
+		});
+	};
+
+
+	Promise.all([p1(), p2()]).then(function(){
 		res.render('home',context);
 	});
+
 });
-
-
 
 
 
@@ -58,7 +93,6 @@ app.get('/', function(req, res, next){
 app.listen(62672, function(){
 	console.log('Server started on Port 62672...');
 });
-
 
 
 
